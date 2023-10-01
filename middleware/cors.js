bf6c325f15031f6fab8794916aa1fbe18ba8b88d
pamudithaKekulandara <pkekulandera@gmail.com>
2023-10-01 08:18:05 +0530

    backend and frontend url validation

---
+++ middleware/cors.js
@@ -0,0 +1,14 @@
+const corsMiddleware = (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type')
+    res.status(200).end()
+  } else {
+    next() // Continue with the main request
+  }
+}
+
+module.exports = { corsMiddleware }
